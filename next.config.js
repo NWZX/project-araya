96fae3c1917498421a2b23bce320f05577cd8433
NWZX <nwzx@users.noreply.github.com>
2021-05-16 21:00:35 -0400

    fix pwa enabled on dev env

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withPWA({
     webpack5: true,
   },
   pwa: {
-    disable: process.env.NODE_ENV === 'development',
+    disable: process.env.NODE_ENV === 'production' ? false : true,
     dest: 'public'
   }
 })
